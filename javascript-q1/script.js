bb950954e4158fc614c368201168ad87899fa63e
shayan.mohammadi <shayan.muhammadi@gmail.com>
2023-10-08 22:57:03 +0330

    hw1-html-q1

---
+++ javascript-q1/script.js
@@ -0,0 +1,60 @@
+// _____Q1_____//
+
+// let num1 = 10 ;
+// let num2 = 20 ;
+// if (num1 > num2){
+// console.log(num1)
+// }else {
+// console.log(num2)
+
+// }
+
+// ____________Q2____________//
+
+// let number = prompt ('Enter a value')
+// if (!isNaN(number)){
+// console.log('is a number')
+// }else{
+// console.log('not a number')
+// }
+// __________________Q3______________
+// let userNumber = prompt("عدد رو وارد کنید")
+// if(userNumber % 2 === 0){
+//    console.log('evan')
+// }else{
+//   console.log('odd')  
+// }
+// ___________Q4___________________
+// let num1 = 50; 
+// let num2 = 11; 
+// let num3 = 30 ;
+// if (num1 > num2 && num1 < num3){
+//     console.log('yes')
+// }else{
+//     console.log('no')
+// }
+
+
+// ___________________Q5_______________
+// for (let i = 0 ; i <= 15; i++){
+//     if(i % 2 === 0 ){
+//         console.log(i +  'is even');
+//     }else{
+//         console.log(i + 'is odd')
+//     }
+// }
+
+// ___________________Q6_______________
+// for (let i = 1 ; i <= 100 ; i++){
+//     if (i % 3 === 0 && i % 5 === 0 ){
+//         console.log('fizzbuzz')
+//     }else{
+//         if (i % 3 === 0 ){
+//             console.log('fizz')
+//         }else if (i % 5 === 0){
+//             console.log('buzz')
+//         }else{
+//             console.log(i)
+//         }
+//     }
+// }
